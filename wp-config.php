c9290b9820d09747bf24fe1e2d44a4319ff1cf03
ser-tav <67864262+ser-tav@users.noreply.github.com>
2021-06-24 12:35:57 +0400

    change db credidentals

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'flynt' );
+define( 'DB_NAME', 'admin_brofist' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin_brofist' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'XOJCcTGNLB' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
