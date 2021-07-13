9db3e66ca7e1492ebd5a109b74fe3128a3b572ba
ser-tav <67864262+ser-tav@users.noreply.github.com>
2021-07-13 15:44:28 +0400

    сf 7

--- wp-content/themes/blankslate-master/functions.php
+++ wp-content/themes/blankslate-master/functions.php
@@ -129,3 +129,15 @@
 <?php 
 	} 
 add_action( 'wp_footer', 'mycustom_wp_footer' );
+
+
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+    $content = str_replace('<br />', '', $content);
+    return $content;
+});
+
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-list-item(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+    return $content;
+});
