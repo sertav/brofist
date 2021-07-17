aa7113da6c67650904ae9b45a8140b13348048a0
ser-tav <67864262+ser-tav@users.noreply.github.com>
2021-07-17 18:55:42 +0400

    news link 3

--- wp-content/themes/blankslate-master/blocks/news.php
+++ wp-content/themes/blankslate-master/blocks/news.php
@@ -18,17 +18,17 @@ $posts = new WP_Query( $args );
             <?php if ( $posts->have_posts() ) : while ( $posts->have_posts() ) : $posts->the_post(); ?>
 
                 <div class="option">
-                    <a href="<?= get_permalink($posts); ?>">
+
                     <div class="img">
                         <?php if ( has_post_thumbnail() ) { the_post_thumbnail('medium'); } ?>
                     </div>
 
                     <div class="text">
-                        <div class="title"><?php the_title(); ?></div>
+                        <div class="title"><a href="<?= get_permalink($posts); ?>"><?php the_title(); ?></a></div>
                         <div class="description"><?php the_excerpt(); ?></div>
                         <div class="date"><?php the_date(); ?></div>
                     </div>
-                    </a>
+
                 </div>
 
             <?php endwhile; endif; wp_reset_query(); ?>
