b39785becf2f87295bc8cf9380bf1147b261a249
ser-tav <67864262+ser-tav@users.noreply.github.com>
2021-07-18 16:34:24 +0400

    fix img pc

--- wp-content/themes/blankslate-master/blocks/brands.php
+++ wp-content/themes/blankslate-master/blocks/brands.php
@@ -24,6 +24,8 @@ $fp_b_items = get_field('fp_b_items');
         $fp_b_pc_bg = get_field('fp_b_pc_bg', $item);
         $fp_b_iphone_logo = get_field('fp_b_iphone_logo', $item);
         $fp_b_iphone_bg = get_field('fp_b_iphone_bg', $item);
+        $fp_b_pc_cover_bg = get_field('fp_b_pc_cover_bg', $item);
+        $fp_b_iphone_cover_bg = get_field('fp_b_iphone_cover_bg', $item);
         ?>
         <div class="brand-info">
             <div class="item">
@@ -81,7 +83,7 @@ $fp_b_items = get_field('fp_b_items');
                     <div class="mac">
                         <img class="main-img" src="<?php bloginfo('template_url'); ?>/assets/images/mac.png" alt="">
                         <div class="mac-background" style="background: <?= $fp_b_pc_bg; ?>;">
-                            <div class="logo"><img src="<?= $fp_b_pc_logo['url']; ?>" alt="<?= esc_attr($fp_b_pc_logo['alt']); ?>"></div>
+                            <div class="logo" style="<?php if ($fp_b_pc_cover_bg == true) echo 'width: 100%'; ?>"><img style="<?php if ($fp_b_pc_cover_bg == true) echo 'height: 100%; object-fit: cover'; ?>" src="<?= $fp_b_pc_logo['url']; ?>" alt="<?= esc_attr($fp_b_pc_logo['alt']); ?>"></div>
                         </div>
                     </div>
 
