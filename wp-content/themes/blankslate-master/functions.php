e5443cf7796761a81ff605e99efb75867b782d3b
ser-tav <67864262+ser-tav@users.noreply.github.com>
2021-07-19 17:33:29 +0400

    undeleteable 10 fix

--- wp-content/themes/blankslate-master/functions.php
+++ wp-content/themes/blankslate-master/functions.php
@@ -198,14 +198,11 @@ function more_less()
 }
 add_action('wp_footer', 'more_less', 100);
 
-$role = get_role('administrator');
-$role->add_cap('delete_pages');
-$role->add_cap('delete_others_pages');
-$role->add_cap('delete_published_pages');
 
 function prevent_default_theme_deletion($allcaps, $caps, $args) {
-    $post_id = 21;
-    if ( isset( $args[0] ) && isset( $args[2] ) && $args[2] == $post_id && $args[0] == 'delete_post' ) {
+    $post_id_eng = 21;
+    $post_id_rus = 427;
+    if ( isset( $args[0] ) && isset( $args[2] ) && $args[2] == $post_id_eng || $args[2] == $post_id_rus && $args[0] == 'delete_post' ) {
         $allcaps[ $caps[0] ] = false;
     }
     return $allcaps;
