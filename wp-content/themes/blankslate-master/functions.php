b55d38d3e206b92a0e5f1ca8d7bebed1f6977d7c
ser-tav <67864262+ser-tav@users.noreply.github.com>
2021-07-19 17:46:46 +0400

    add script 4

--- wp-content/themes/blankslate-master/functions.php
+++ wp-content/themes/blankslate-master/functions.php
@@ -51,6 +51,7 @@ function brofist_load_scripts()
 
         wp_enqueue_script('jquery-js');
         wp_enqueue_script('swiper');
+        wp_enqueue_script('owl-carousel');
         wp_enqueue_script('TweenMax');
         wp_enqueue_script('wow');
         wp_enqueue_script('main');
